7ac9ad5a5820c3bc0fd1e0acdf6ac17d4104ea06
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2024-12-28 09:45:54 +0530

    User.java

--- User.java
+++ User.java
@@ -1,49 +1,39 @@
 import java.util.ArrayList;
-class User {
+
+public class User {
     private String username;
     private String password;
     private double balance;
-    private ArrayList<String> transactionHistory;
+    private ArrayList<Transaction> transactionHistory;
 
     public User(String username, String password, double balance) {
         this.username = username;
         this.password = password;
         this.balance = balance;
         this.transactionHistory = new ArrayList<>();
-        this.transactionHistory.add("Account created with balance: Rs." + balance);
+        this.transactionHistory.add(new Transaction(username, "Initial Amount", balance));
     }
-
     public String getUsername() {
         return username;
     }
-
-    public boolean checkPassword(String enteredPassword) {
-        return this.password.equals(enteredPassword);
+    public String getPassword() {
+        return password;
     }
-
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public double getBalance() {
         return balance;
     }
-
-    public void deposit(double amount) {
-        balance += amount;
-        transactionHistory.add("Deposited: Rs." + amount + " | New Balance: Rs." + balance);
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
-
-    public void withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-            transactionHistory.add("Withdrew: Rs." + amount + " | New Balance: Rs." + balance);
-        } else {
-            transactionHistory.add("Failed withdrawal attempt: Insufficient funds.");
-            System.out.println("Insufficient funds. Withdrawal failed.");
-        }
+    public ArrayList<Transaction> getTransactionHistory() {
+        return transactionHistory;
     }
-
-    public void viewTransactionHistory() {
-        System.out.println("Transaction History for " + username + ":");
-        for (String transaction : transactionHistory) {
-            System.out.println(transaction);
-        }
+    public void addTransaction(Transaction transaction) {
+        this.transactionHistory.add(transaction);
     }
 }
+
+
