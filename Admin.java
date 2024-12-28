3664308bb5f3158a048a437abbb7cca250ea63b5
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2024-12-28 09:44:32 +0530

    Admin.java

--- Admin.java
+++ Admin.java
@@ -1,15 +1,29 @@
+import java.util.ArrayList;
+
 public class Admin {
-    private String  id;
+    private String id;
     private String password;
+    private ArrayList<Transaction> transactionHistory;
+
     public Admin(String id, String password) {
         this.id = id;
         this.password = password;
+        this.transactionHistory = new ArrayList<>();
     }
+
     public String getId() {
         return id;
     }
-    public boolean checkPassword(String enteredPassword) {
-        return this.password.equals(enteredPassword);
-    }
-}
 
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<Transaction> getTransactionHistory() {
+        return transactionHistory;
+    }
+    public void addTransaction(Transaction transaction) {
+        this.transactionHistory.add(transaction);
+    }
+
+}
