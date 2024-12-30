c719428917c11c9b245ae70858b476f2944dbb71
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2024-12-30 14:30:28 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class ATM {
     static ArrayList<User> users = new ArrayList<>();
-    static ArrayList<Notes> notes = new ArrayList<>();
     public static int notes2000 = 0;
     public static int notes500 = 0;
     public static int notes200 = 0;
     public static int notes100 = 0;
+
 
     public static void start() {
         Scanner scanner = new Scanner(System.in);
@@ -36,8 +36,7 @@ public class ATM {
                 } else {
                     System.out.println("User not found. Please check the username and try again.");
                 }
-            }
-            else if (login == 3) {
+            } else if (login == 3) {
                 System.out.println("Exiting the program.");
                 break;
             } else {
@@ -54,31 +53,58 @@ public class ATM {
         }
         return null;
     }
-
     public static int getTotalATMBalance() {
         return (notes2000 * 2000) + (notes500 * 500) + (notes200 * 200) + (notes100 * 100);
     }
 
 
     public static boolean withdrawFromATM(int amount, Notes2000 notes2000, Notes500 notes500, Notes200 notes200, Notes100 notes100) {
+
         int totalAvailableAmount = notes2000.getNote() * notes2000.getCount() +
                 notes500.getNote() * notes500.getCount() +
                 notes200.getNote() * notes200.getCount() +
                 notes100.getNote() * notes100.getCount();
 
-        if (amount <= totalAvailableAmount) {
-            return true;
-        } else {
+
+        if (amount > totalAvailableAmount) {
             return false;
         }
+        int remainingAmount = amount;
+        int num2000NotesToWithdraw = Math.min(remainingAmount / notes2000.getNote(), notes2000.getCount());
+        remainingAmount -= num2000NotesToWithdraw * notes2000.getNote();
+        int num500NotesToWithdraw = Math.min(remainingAmount / notes500.getNote(), notes500.getCount());
+        remainingAmount -= num500NotesToWithdraw * notes500.getNote();
+        int num200NotesToWithdraw = Math.min(remainingAmount / notes200.getNote(), notes200.getCount());
+        remainingAmount -= num200NotesToWithdraw * notes200.getNote();
+        int num100NotesToWithdraw = Math.min(remainingAmount / notes100.getNote(), notes100.getCount());
+        remainingAmount -= num100NotesToWithdraw * notes100.getNote();
+        if (remainingAmount == 0) {
+            System.out.println("Withdrawal successful. Denomination");
+            if (num2000NotesToWithdraw > 0) {
+                System.out.println("Rs. 2000 x " + num2000NotesToWithdraw);
+            }
+            if (num500NotesToWithdraw > 0) {
+                System.out.println("Rs. 500 x " + num500NotesToWithdraw);
+            }
+            if (num200NotesToWithdraw > 0) {
+                System.out.println("Rs. 200 x " + num200NotesToWithdraw);
+            }
+            if (num100NotesToWithdraw > 0) {
+                System.out.println("Rs. 100 x " + num100NotesToWithdraw);
+            }
+            return true;
+        }
+        return false;
     }
 
+
+
     public static double depositAmountToATM(Notes2000 notes2000, Notes500 notes500, Notes200 notes200, Notes100 notes100) {
-            return notes2000.getNote() * notes2000.getCount() +
-                    notes500.getNote() * notes500.getCount() +
-                    notes200.getNote() * notes200.getCount() +
-                    notes100.getNote() * notes100.getCount();
-        }
+        return notes2000.getNote() * notes2000.getCount() +
+                notes500.getNote() * notes500.getCount() +
+                notes200.getNote() * notes200.getCount() +
+                notes100.getNote() * notes100.getCount();
     }
+}
 
 
