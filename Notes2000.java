06f9f76c50f4e4888efbe707ef79ee717f24a70a
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2024-12-30 14:34:56 +0530

    Update Notes2000.java

--- Notes2000.java
+++ Notes2000.java
@@ -1,7 +1,6 @@
 public class Notes2000 extends Notes {
     public Notes2000(int Note, int Count) {
-        this.setNote(Note);
-        this.setCount(Count);
+        super(Note, Count);
     }
 
 }
