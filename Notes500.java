79b1f1f1135d98f5da729e71af4f34f0a3a81c12
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2024-12-30 14:35:20 +0530

    Update Notes500.java

--- Notes500.java
+++ Notes500.java
@@ -1,8 +1,6 @@
 public class Notes500 extends Notes {
     public Notes500(int Note, int Count) {
-        this.setNote(Note);
-        this.setCount(Count);
+        super(Note, Count);
     }
 
 }
-
