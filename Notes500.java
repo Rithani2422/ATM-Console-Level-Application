d9dc1e33a139aa4e44127453d39ba7c91a45d167
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-01-01 19:49:23 +0530

    Update Notes500.java

--- Notes500.java
+++ Notes500.java
@@ -1,6 +1,10 @@
+// Define a class Notes500 that extends the Notes class
 public class Notes500 extends Notes {
+
+    // Constructor for the Notes500 class that takes two parameters: Note and Count
     public Notes500(int Note, int Count) {
+
+        // Call the superclass (Notes) constructor with the provided Note and Count values
         super(Note, Count);
     }
-
 }
