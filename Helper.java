285a863a0df62d59c3224932bd3e9c61f121a39d
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-01-01 19:46:44 +0530

    Update Helper.java

--- Helper.java
+++ Helper.java
@@ -1,5 +1,13 @@
 public class Helper {
+    // Declares a public class named 'Helper'
+    // the main entry point for the program
+
     public static void main(String[] args) {
+        // The main method: the execution of the program starts here
+        // 'String[] args' can be used to pass command-line arguments
+
         ATM.start();
+        // Calls the 'start' method from the 'ATM' class
+        // begins the functionality of the ATM program
     }
 }
