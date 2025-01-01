2a5bb0ba75352b29439e18b799953fd40d3a6c6b
Rithani Rajkumar <163626629+Rithani2422@users.noreply.github.com>
2025-01-01 19:48:51 +0530

    Update Notes2000.java

--- Notes2000.java
+++ Notes2000.java
@@ -1,6 +1,10 @@
+// Define a class Notes2000 that extends the Notes class
 public class Notes2000 extends Notes {
+
+    // Constructor for the Notes2000 class that takes two parameters: Note and Count
     public Notes2000(int Note, int Count) {
+
+        // Call the superclass (Notes) constructor with the provided Note and Count values
         super(Note, Count);
     }
-
 }
